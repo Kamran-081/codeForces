e37a1e8305e1e75274e05921581857aba0b566b9
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-01-28 23:56:38 +0530

    Add hollow triangle pattern printing program

---
+++ Day3/hollowTriangle.cpp
@@ -0,0 +1,35 @@
+//      * 
+//     * * 
+//    *   * 
+//   *     * 
+//  *       * 
+// * * * * * * 
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int s = 1; s <= n - i; s++)
+            cout << " ";
+
+        if (i == 1)
+            cout << "*";
+        else if (i == n) {
+            for (int j = 1; j <= i; j++)
+                cout << "* ";
+        } else {
+            cout << "*";
+            for (int j = 1; j <= 2*i - 3; j++)
+                cout << " ";
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
