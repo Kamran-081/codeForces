160d512721a2c97c58985ea8352038a7537853e3
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-02-17 23:29:37 +0530

    Create Print Factors - II.cpp

---
+++ Day4/Print Factors - II.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void printFactors(int N) {
+    for(int i = N; i >= 1; i--) {
+        if(N % i == 0) {
+            cout << i << " ";
+        }
+    }
+}
+
+int main() {
+    int N;
+    cin >> N;
+    printFactors(N);
+    return 0;
+}
